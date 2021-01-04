6f555814eb330d7c30760e7eb50d1a3cd878bfa7
Didnelpsun <48906416+Didnelpsun@users.noreply.github.com>
2021-01-04 23:50:37 +0800

    主页版块跳转

--- Templates/Navigation/Navigation.master.cs
+++ Templates/Navigation/Navigation.master.cs
@@ -8,10 +8,54 @@ namespace AchieveDream.Templates.Navigation
     {
         protected void Page_Init(object sender, EventArgs e)
         {
-            HttpCookie work = Request.Cookies["work"];
-            if(work != null)
-            {
+            SwitchHandle();
+        }
 
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SwitchHandle();
+        }
+
+        protected void SwitchHandle()
+        {
+            HttpCookie work = Request.Cookies["work"];
+            if (work != null)
+            {
+                if (work["switchIndex"].ToString() == "0")
+                {
+                    SwitchHome.CssClass = "switchButton switchChoose cc";
+                    SwitchForum.CssClass = "switchButton cc";
+                    SwitchCustom.CssClass = "switchButton cc";
+                    SwitchMine.CssClass = "switchButton cc";
+                }
+                else if (work["switchIndex"].ToString() == "1")
+                {
+                    SwitchHome.CssClass = "switchButton cc";
+                    SwitchForum.CssClass = "switchButton switchChoose cc";
+                    SwitchCustom.CssClass = "switchButton cc";
+                    SwitchMine.CssClass = "switchButton cc";
+                }
+                else if (work["switchIndex"].ToString() == "2")
+                {
+                    SwitchHome.CssClass = "switchButton cc";
+                    SwitchForum.CssClass = "switchButton cc";
+                    SwitchCustom.CssClass = "switchButton switchChoose cc";
+                    SwitchMine.CssClass = "switchButton cc";
+                }
+                else if (work["switchIndex"].ToString() == "3")
+                {
+                    SwitchHome.CssClass = "switchButton cc";
+                    SwitchForum.CssClass = "switchButton cc";
+                    SwitchCustom.CssClass = "switchButton cc";
+                    SwitchMine.CssClass = "switchButton switchChoose cc";
+                }
+                else
+                {
+                    SwitchHome.CssClass = "switchButton switchChoose cc";
+                    SwitchForum.CssClass = "switchButton cc";
+                    SwitchCustom.CssClass = "switchButton cc";
+                    SwitchMine.CssClass = "switchButton cc";
+                }
             }
             else
             {
@@ -27,38 +71,30 @@ namespace AchieveDream.Templates.Navigation
             string text = ((Button)sender).Text.ToString().Trim();
             if(text == "首页")
             {
-                ViewState["index"] = 0;
-                SwitchHome.CssClass = "switchButton switchChoose cc";
-                SwitchForum.CssClass = "switchButton cc";
-                SwitchCustom.CssClass = "switchButton cc";
-                SwitchMine.CssClass = "switchButton cc";
+                HttpCookie work = new HttpCookie("work");
+                work["switchIndex"] = "0";
+                Response.Cookies.Add(work);
                 Response.Redirect("../../Pages/Home/Home.aspx");
             }
             else if(text == "论坛")
             {
-                ViewState["index"] = 1;
-                SwitchHome.CssClass = "switchButton cc";
-                SwitchForum.CssClass = "switchButton switchChoose cc";
-                SwitchCustom.CssClass = "switchButton cc";
-                SwitchMine.CssClass = "switchButton cc";
+                HttpCookie work = new HttpCookie("work");
+                work["switchIndex"] = "1";
+                Response.Cookies.Add(work);
                 Response.Redirect("../../Pages/Forum/Forum.aspx");
             }
             else if(text == "定制")
             {
-                ViewState["index"] = 2;
-                SwitchHome.CssClass = "switchButton cc";
-                SwitchForum.CssClass = "switchButton cc";
-                SwitchCustom.CssClass = "switchButton switchChoose cc";
-                SwitchMine.CssClass = "switchButton cc";
+                HttpCookie work = new HttpCookie("work");
+                work["switchIndex"] = "2";
+                Response.Cookies.Add(work);
                 Response.Redirect("../../Pages/Custom/Custom.aspx");
             }
             else if(text == "我的")
             {
-                ViewState["index"] = 3;
-                SwitchHome.CssClass = "switchButton cc";
-                SwitchForum.CssClass = "switchButton cc";
-                SwitchCustom.CssClass = "switchButton cc";
-                SwitchMine.CssClass = "switchButton switchChoose cc";
+                HttpCookie work = new HttpCookie("work");
+                work["switchIndex"] = "3";
+                Response.Cookies.Add(work);
                 Response.Redirect("../../Pages/Mine/Mine.aspx");
             }
         }
